ce8dc2aecac65831bcf5b246324401387f12ec17
Mohammad Mohsin Ahmad <mdm01046@gmail.com>
2024-11-08 20:01:43 +0530

    back4app url  is added  code is good

--- URL.js
+++ URL.js
@@ -1,1 +1,2 @@
-export default URL_path = 'http://192.168.31.155:8800'
+// export default URL_path = 'http://192.168.31.155:8800'
+export default URL_path = 'https://freshandfreshbackend-j98njjva.b4a.run/'
